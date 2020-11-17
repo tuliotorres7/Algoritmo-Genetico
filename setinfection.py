f9fbe5b6c1088fcc7e145af00d5781f12b67d454
tuliotorres7 <40638626+tuliotorres7@users.noreply.github.com>
2020-11-17 13:26:39 -0300

    Update setinfection.py

--- setinfection.py
+++ setinfection.py
@@ -11,7 +11,7 @@ days =113
 n = 500
 beta = 0.3
 gamma = 0.3
-g = nx.erdos_renyi_graph(n, 0.5)
+g = nx.erdos_renyi_graph(n, 0.1)
 a = int()
 
 
@@ -21,10 +21,11 @@ a = int()
 SIRModel = ep.SIRModel(g)
 # Model Configuration
 cfg = mc.Configuration()
-cfg.add_model_parameter('beta', beta)
-cfg.add_model_parameter('gamma', gamma)
-cfg.add_model_parameter("fraction_infected", 0.3)
+cfg.add_model_parameter('beta', 0.3)
+cfg.add_model_parameter('gamma',0.05)
+cfg.add_model_parameter("fraction_infected", 0.03)
 SIRModel.set_initial_status(cfg)
+
 
 
 # Ready CSV
@@ -55,25 +56,27 @@ data = sirSj
 varbound=np.array([[0,1]]*2)
 
 def fitness(x):
-    #SIRModel.reset()
+    SIRModel.reset()
+
     cfg.add_model_parameter('beta', x[0])
     cfg.add_model_parameter('gamma', x[1])
+    cfg.add_model_parameter("fraction_infected", 0.03)
     SIRModel.set_initial_status(cfg)
     iterations = SIRModel.iteration_bunch(days)
     print(iterations)
     a = 0
     Igerado.clear()
     Rgerado.clear()
-    for x in iterations:
-        matriz_gerada[a][0] = x['node_count'][0]
-        matriz_gerada[a][1] = x['node_count'][1]
-        matriz_gerada[a][2] = x['node_count'][2]
-        Igerado.append(x['node_count'][1])
-        Rgerado.append(x['node_count'][2])
+    for v in iterations:
+        matriz_gerada[a][0] = v['node_count'][0]
+        matriz_gerada[a][1] = v['node_count'][1]
+        matriz_gerada[a][2] = v['node_count'][2]
+        Igerado.append(v['node_count'][1])
+        Rgerado.append(v['node_count'][2])
         a = a + 1
   
-    print(matriz_gerada)
-    print(Igerado)
+    #print(matriz_gerada)
+    #print(Igerado)
     print(iterations)
 
     mseI = mean_squared_error(sirI, Igerado)
@@ -84,6 +87,15 @@ def fitness(x):
     return f
 
 
-GaModel= ga(function= fitness,dimension=2,variable_type='real',variable_boundaries=varbound)
+algorithm_param = {'max_num_iteration': 100,\
+                   'population_size':100,\
+                   'mutation_probability':0.05,\
+                   'elit_ratio': 0.01,\
+                   'crossover_probability': 0.8,\
+                   'parents_portion': 0.1,\
+                   'crossover_type':'uniform',\
+                   'max_iteration_without_improv': 20}
+
+GaModel= ga(function= fitness,dimension=2,variable_type='real',variable_boundaries=varbound, algorithm_parameters= algorithm_param)
 
 GaModel.run()
